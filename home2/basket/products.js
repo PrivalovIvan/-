e771eb9c27df319514947b6b66d7a6bc3bc4ddcc
geekbrains <mamamia89678524930@gmail.com>
2022-01-22 12:41:18 +0500

    dzjs

---
+++ home2/basket/products.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const products = [
+    new ProductDTO(
+        0,
+        '0.png',
+        'Product 0',
+        'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.',
+        52.22,
+    ),
+    new ProductDTO(
+        1,
+        '1.png',
+        'Product 1',
+        'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.',
+        32.12,
+    ),
+    new ProductDTO(
+        2,
+        '2.png',
+        'Product 2',
+        'Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.',
+        82.87,
+    ),
+    new ProductDTO(
+        3,
+        '3.png',
+        'Product 3',
+        'In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.',
+        22.39,
+    ),
+    new ProductDTO(
+        4,
+        '4.png',
+        'Product 4',
+        'Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.',
+        92.99,
+    ),
+    new ProductDTO(
+        5,
+        '5.png',
+        'Product 5',
+        'Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem.',
+        12.55,
+    ),
+];
